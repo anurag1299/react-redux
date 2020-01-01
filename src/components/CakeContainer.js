55f665a6277565bada31be599e7608a576b098b1
Alkatraz <anuragbmore@gmail.com>
2020-01-01 23:37:08 +0530

    Learning react-redux (done basic connections)

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { buyCake } from '../redux/cake/cakeActions'
+import { connect } from 'react-redux'
+
+function CakeContainer(props) {
+    return (
+        <div>
+            <h2>No of cakes = {props.numOfCakes}</h2>
+            <button onClick={props.buyCake}>Buy cake</button>
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        numOfCakes : state.noOfCakes
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        buyCake : () => dispatch(buyCake())
+    }
+}
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer)
